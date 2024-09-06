e2b9fbc4827d735868e62b70d5b8863570629038
modaniharsh-iiitb <harsh.modani@iiitb.ac.in>
2024-09-06 20:39:47 +0530

    Worked out procedure

--- solution.py
+++ solution.py
@@ -2,6 +2,8 @@ import sys
 import numpy as np
 from input_interpret import interpret
 
+import time
+
 class Tableau:
 
     def __init__(self, obj, num_vars, unit_cost, constraints):
@@ -16,6 +18,9 @@ class Tableau:
         self.slack_vars = 0
         self.slack_var_counter = 0
 
+        self.build_tableau()
+
+    def build_tableau(self):
         # count the number of slack variables needed
         for c in self.constraints:
             if c[1] == '<=' or c[1] == '>=':
@@ -25,13 +30,11 @@ class Tableau:
         self.constraint_matrix = np.zeros((len(self.constraints), self.num_vars + self.slack_vars + 1))
         for i in range(len(self.constraints)):
             # the coefficient vector is the list of coefficients of the constraint including slack variables and the constant
-            print(self.constraints[i][2])
             coeff_vector = np.array(self.constraints[i][2])
-            print(coeff_vector)
             # pad the coefficient vector with zeros to account for slack variables and the constant
             coeff_vector = np.pad(coeff_vector, (0, self.slack_vars + 1), 'constant', constant_values=(0))
             # add the constant to the end of the coefficient vector
-            coeff_vector[self.num_vars + self.slack_vars] = self.constraints[i][0]
+            coeff_vector[-1] = self.constraints[i][0]
 
             # in case of <= inequality, add one slack variable to the coefficient vector
             if self.constraints[i][1] == '>=':
@@ -45,7 +48,42 @@ class Tableau:
 
             self.constraint_matrix[i] = coeff_vector
 
+        objective_row = np.pad((self.obj * np.array(self.unit_cost)), (0, self.slack_vars + 1), 'constant', constant_values=(0))
+        self.constraint_matrix = np.vstack([self.constraint_matrix, objective_row])
         print(self.constraint_matrix)
+
+    def pivot(self, a, b):
+        self.constraint_matrix[b] /= self.constraint_matrix[b, a]
+        for i in range(len(self.constraint_matrix)):
+            if i == b:
+                continue
+            self.constraint_matrix[i] -= self.constraint_matrix[i, a] * self.constraint_matrix[b]
+
+    def solve(self):
+        # # check for infeasibility
+        # if np.all(self.constraint_matrix[:-1, -1] >= 0): # this is NOT YET the correct boolean
+        #     return ("INFEASIBLE", None)
+        
+        # # check for unboundedness
+        # if np.all(self.constraint_matrix[-1, :-1] <= 0): # this is NOT YET the correct boolean
+        #     return ("UNBOUNDED", None)
+
+        while (True):
+            # if all values in the last row are non-negative, the solution is optimal
+            if np.all(self.constraint_matrix[-1] >= 0):
+                break
+            # the pivot column (corresponding to the entering variable) is the first negative value in the last row
+            a = np.argwhere(self.constraint_matrix[-1] < 0)[0][0]
+            print(a)
+            # the pivot row (corresponding to the leaving variable) is the row with the smallest ratio of the constant to the pivot column
+            b = np.argmin(self.constraint_matrix[:-1, -1] / self.constraint_matrix[:-1, a])
+            print(b)
+            self.pivot(a, b)
+            print(self.constraint_matrix)
+
+            time.sleep(1)
+
+        return ("OPTIMAL", self.constraint_matrix[-1, -1])
 
 if __name__ == '__main__':
     n = len(sys.argv)
@@ -55,8 +93,7 @@ if __name__ == '__main__':
 
     filename = sys.argv[1]
     obj, num_vars, unit_cost, constraints = interpret(filename)
-    print(obj)
-    print(num_vars)
-    print(unit_cost)
-    print(constraints)
     t = Tableau(obj, num_vars, unit_cost, constraints)
+    k = t.solve()
+    print(k[0])
+    print(k[1])
