a598be0376704cdae537dd9a58273c08b5a4b73f
dh-r-uv <Dhruv.Kothari@iiitb.ac.in>
2024-09-08 00:45:58 +0530

    Positional input added

--- input_interpret.py
+++ input_interpret.py
@@ -1,4 +1,4 @@
-import fractions
+import fractions    
 
 # function to interpret the input file and return the objective function, number of variables, unit cost vector and constraints
 def interpret(filename):
@@ -12,17 +12,31 @@ def interpret(filename):
         obj = 1 if obj.lower() == "min" else -1
 
         # the next line contains the unit cost vector, i.e. coefficients of the objective function
-        unit_cost = list(map(float, lines[1].split()))
+        # unit_cost = list(map(f, lines[1].split()))
+        unit_cost = list(map(fractions.Fraction, lines[1].split()))
 
         # the next lines contain the constraints - can be equalitites or inequalities
         constraints = lines[2:]
         # each line contains the constant, followed by the type of constraint, followed by the coefficients of the variables
-        for i in range(len(constraints)):
-            constraint = constraints[i].strip().split()
-            constraint[0] = float(constraint[0])
-            constraint[2] = list(map(float, constraint[2:]))
-            constraints[i] = constraint
+
+        act_constraints = []
+
+        for constraint in constraints:
+            constraint = constraint.strip().split()
+            constant = fractions.Fraction(constraint[0])
+            constraint_type = constraint[1]
+            temp = [fractions.Fraction(0) for i in range(num_vars)]
+            # 3 >= 2 2@1
+            for i in range(2, len(constraint)):
+                if('@' in constraint[i]):
+                    temp[int(constraint[i].split('@')[1])-1] = fractions.Fraction(constraint[i].split('@')[0])
+                else:
+                    temp[i-2] = fractions.Fraction(constraint[i])
+            act_constraints.append([constant, constraint_type, temp])
+
 
         # the assumed constraints are that all variables are non-negative
-
-        return obj, num_vars, unit_cost, constraints
+        print(obj, num_vars, unit_cost, act_constraints)
+        return obj, num_vars, unit_cost, act_constraints
+    
+interpret('inp.csv')
